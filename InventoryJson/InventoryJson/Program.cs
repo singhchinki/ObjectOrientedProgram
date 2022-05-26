87865de4a9f4511a784e74299f01ebd5b5edfe56
chinki kumari singh <singhchinkikumari@gmail.com>
2022-05-26 16:54:22 +0530

    [chinki] Json Inventory Data Mangement of Rice, Pulses and Wheats

---
+++ InventoryJson/InventoryJson/Program.cs
@@ -0,0 +1,2 @@
+﻿InventoryJson.jsonInventoryMain json = new InventoryJson.jsonInventoryMain();
+json.convert(@"F:\BridgeLabzFellowship\ObjectOrientedProgram\InventoryJson\InventoryJson\Inventory.json");
---
+++ InventoryJson/InventoryJson/jsonInventoryMain.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryJson
+{
+    internal class jsonInventoryMain
+    {
+        InventoryModel model = new InventoryModel();
+        List<InventoryModel> list = new List<InventoryModel>();
+        public void convert(string jFilePath)
+        {
+            using(StreamReader file = new StreamReader(jFilePath))
+            {
+                var json = file.ReadToEnd();
+                var items = JsonConvert.DeserializeObject<List<InventoryModel>>(json);
+                foreach(var objects in items)
+                {
+                    Console.WriteLine(objects.Name + " " + objects.Price + " " + objects.Weight);
+                }
+            }
+        }
+    }
+}
